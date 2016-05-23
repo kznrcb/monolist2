9b238e20b46d5fdd59659c449baa1a8c342f6288
kznrcb <kazunorichiba+cloud9@gmail.com>
2016-05-23 16:53:54 +0000

    add rank

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,12 @@
 class RankingController < ApplicationController
 
 def have
-    @title = "have"
+    @title = "had"
     @items = Have.group(:item_id).order('count_all desc').count.keys.map{|id| Item.find(id)}.first(10)
 end
 
 def want
-    @title = "want"
+    @title = "wanted"
     @items = Want.group(:item_id).order('count_all desc').count.keys.map{|id| Item.find(id)}.first(10)
 end
 
